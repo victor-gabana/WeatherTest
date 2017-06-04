98add9efd0849ef402c610d3178335ed820aca07
Victor Gabana <a-vgabana@hotels.com>
2017-06-04 18:46:24 +0100

    Initial Commit

---
+++ WeatherLloydsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherLloydsTest
+//
+//  Created by Divide by Zero on 04/06/2017.
+//  Copyright © 2017 Divide by Zero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
